5b8f04f45168652f47b69e0b0eceaab4451a288d
Darjan Cata <cata_dc98@yahoo.com>
2022-10-24 10:18:02 +0300

    Update Theme, Colors & Typography

--- app/src/main/java/com/pose/move/ui/theme/Type.kt
+++ app/src/main/java/com/pose/move/ui/theme/Type.kt
@@ -2,33 +2,66 @@ package com.pose.move.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.pose.move.R
 
-// Set of Material typography styles to start with
+private val baiJamjuree = FontFamily(
+    Font(R.font.bai_jamjuree_regular),
+    Font(R.font.bai_jamjuree_medium, FontWeight.Medium),
+    Font(R.font.bai_jamjuree_semibold, FontWeight.SemiBold),
+    Font(R.font.bai_jamjuree_bold, FontWeight.Bold)
+)
+
 val Typography = Typography(
+    titleLarge = TextStyle(
+        fontFamily = baiJamjuree,
+        fontWeight = FontWeight.Bold,
+        fontSize = 32.sp,
+        lineHeight = 40.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = baiJamjuree,
+        fontWeight = FontWeight.Medium,
+        fontSize = 20.sp,
+        lineHeight = 28.sp
+    ),
+    titleSmall = TextStyle(
+        fontFamily = baiJamjuree,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 18.sp,
+        lineHeight = 24.sp
+    ),
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = baiJamjuree,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp,
+        lineHeight = 24.sp
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = baiJamjuree,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        lineHeight = 20.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = baiJamjuree,
         fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        lineHeight = 16.sp
+    ),
+    labelLarge = TextStyle(
+        fontFamily = baiJamjuree,
+        fontWeight = FontWeight.Bold,
         fontSize = 16.sp,
         lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+    labelMedium = TextStyle(
+        fontFamily = baiJamjuree,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 12.sp,
+        lineHeight = 16.sp
     )
-    */
 )
