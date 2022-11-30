e678f30f60ff9ae4b1b271259e54cbfa19aed0bd
Darjan Cata <cata_dc98@yahoo.com>
2022-11-30 08:52:58 +0200

    Update headers UI

--- app/src/main/java/com/pose/move/feature/home/availablescooters/SectionsHeader.kt
+++ app/src/main/java/com/pose/move/feature/home/availablescooters/SectionsHeader.kt
@@ -3,33 +3,29 @@ package com.pose.move.feature.home.availablescooters
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.pose.move.feature.home.availablescooters.item.AvailableScootersListItem
-import kotlinx.coroutines.launch
 
 @Composable
 fun SectionsHeader(
     modifier: Modifier = Modifier,
     headerItems: List<AvailableScootersListItem.Header>,
     sectionsListState: LazyListState = rememberLazyListState(),
-    onItemClick: suspend (header: AvailableScootersListItem.Header) -> Unit
+    onItemClick: (header: AvailableScootersListItem.Header) -> Unit
 ) {
-    val coroutineScope = rememberCoroutineScope()
-
     LazyRow(
         modifier = modifier,
         horizontalArrangement = Arrangement.spacedBy(4.dp),
@@ -39,12 +35,13 @@ fun SectionsHeader(
         items(headerItems) { header ->
             Button(
                 modifier = Modifier
-                    .size(32.dp),
-                shape = CircleShape,
+                    .height(32.dp)
+                    .width(64.dp),
+                shape = MaterialTheme.shapes.large,
                 border = BorderStroke(2.dp, MaterialTheme.colorScheme.onBackground),
                 colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.primaryContainer),
                 contentPadding = PaddingValues(0.dp),
-                onClick = { coroutineScope.launch { onItemClick(header) } }
+                onClick = { onItemClick(header) }
             ) {
                 Text(
                     text = header.letter.toString(),
